e2e030e65ca61436a4e8fbb41e0c0a7d4fe937ed
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2024-03-23 13:22:21 +0200

    task 4.2 done. all the functionality done.

--- app/io/input.py
+++ app/io/input.py
@@ -1,9 +1,24 @@
 
 def input_text():
+    """
+    Prompts the user to input text from the console.
+
+    Returns:
+    str: The text entered by the user.
+    """
     text = input("Enter text: ")
     return text
 
 def read_from_file(file_path):
+    """
+    Reads content from a file.
+
+    Args:
+    file_path (str): The path to the file.
+
+    Returns:
+    str: The content of the file.
+    """
     try:
         with open(file_path, 'r') as file:
             content = file.read()
@@ -16,8 +31,17 @@ def read_from_file(file_path):
 import pandas as pd
 
 def read_with_pandas(file_path):
+    """
+    Reads data from a file using pandas.
+
+    Args:
+    file_path (str): The path to the file.
+
+    Returns:
+    DataFrame: The data read from the file.
+    """
     try:
-        data = pd.read_txt(file_path)
+        data = pd.read_csv(file_path)
         return data
     except FileNotFoundError:
         return "File not found"
--- app/io/output.py
+++ app/io/output.py
@@ -1,11 +1,29 @@
 
 def print_to_console(text):
+    """
+    Prints text to the console.
+
+    Args:
+    text (str): The text to be printed.
+    """
     print(text)
 
-def write_to_file(file_path, content):
-    try:
-        with open(file_path, 'w') as file:
-            file.write(content)
-        return "Content has been successfully written to the file."
-    except Exception as e:
-        return f"Error: {str(e)}"
+def write_to_file(text):
+    """
+    Writes content to a file using Python's built-in capabilities.
+
+    Args:
+    text (str): The text to be written to the file.
+    """
+
+    with open("file_for_output.txt", 'w') as file:
+        file.write(text)
+
+def write_pandas(data):
+    """
+    Writes content to a file using Pandas.
+
+     Args:
+    data (pandas.DataFrame): Input value that will be written to file.
+    """
+    data.to_csv("file_for_output.csv")
