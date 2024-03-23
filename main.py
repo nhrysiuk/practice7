cd8f791cc59bef4caac1fb9ad33c619dc9d51308
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2024-03-23 13:22:36 +0200

    task 4.2 done. all the functionality done.

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+import app.io.input as input
+import app.io.output as output
 
 
 def main():
-    pass
+    input_1 = input.input_text()
+    input_2 = input.read_from_file('input.txt')
+    input_3 = input.read_with_pandas('customers.csv')
+
+    output.print_to_console(input_1)
+    output.write_to_file(input_2)
+    output.write_pandas(input_3)
+
 
 if __name__ == "__main__":
     main()
